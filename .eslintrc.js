cac72aa96be3b9cfb52e41f461d7a193c7913bdd
Austin Zhu <austinzhu666@gmail.com>
2022-09-06 04:16:47 +0900

    chore: update hardhat config & setup monorepo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,16 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: 'standard-with-typescript',
   parser: '@typescript-eslint/parser',
-  plugins: ["@typescript-eslint"],
-  overrides: [
-  ],
+  plugins: ['@typescript-eslint'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
-  rules: {
-  }
+  rules: {},
 }
