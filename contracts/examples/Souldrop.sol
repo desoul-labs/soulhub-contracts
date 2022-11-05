e56794b035fcd1cd963edc7d92cd37d80258afca
Austin Zhu <austinzhu666@gmail.com>
2022-11-05 16:31:21 +0900

    feat: add souldrop contract

---
+++ contracts/examples/Souldrop.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: CC0-1.0
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/structs/EnumerableMapUpgradeable.sol";
+
+import "./ERC5727ExampleUpgradeable.sol";
+
+contract Souldrop is ERC721EnumerableUpgradeable, OwnableUpgradeable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+    using EnumerableMapUpgradeable for EnumerableMapUpgradeable.AddressToUintMap;
+
+    CountersUpgradeable.Counter private _tokenIdCounter;
+
+    string private _baseTokenURI;
+
+    ERC5727ExampleUpgradeable private erc5727;
+
+    EnumerableMapUpgradeable.AddressToUintMap private _snapshot;
+
+    function initialize(
+        address owner,
+        uint256 threshold,
+        string memory baseTokenURI,
+        address erc5727Address
+    ) public initializer {
+        __Ownable_init_unchained();
+        __ERC721_init_unchained("Souldrop", "SDP");
+        __ERC721Enumerable_init_unchained();
+        transferOwnership(owner);
+
+        _baseTokenURI = baseTokenURI;
+
+        erc5727 = ERC5727ExampleUpgradeable(erc5727Address);
+        uint256 total = erc5727.emittedCount();
+        for (uint256 i = 0; i < total; i++) {
+            address soul = erc5727.soulOf(i);
+            (bool exist, uint256 val) = _snapshot.tryGet(soul);
+            _snapshot.set(soul, erc5727.valueOf(i) + val);
+        }
+        for (uint256 i = 0; i < _snapshot.length(); i++) {
+            (address soul, uint256 value) = _snapshot.at(i);
+            if (value >= threshold) {
+                _safeMint(soul);
+            }
+        }
+    }
+
+    function _safeMint(address to) internal onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721EnumerableUpgradeable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+        return _baseURI();
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721EnumerableUpgradeable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ test/Souldrop.test.ts
@@ -0,0 +1,40 @@
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
+
+describe('Souldrop', function () {
+  it('mint', async function () {
+    const erc5727Factory = await ethers.getContractFactory('ERC5727ExampleUpgradeable')
+    const erc5727 = await erc5727Factory.deploy()
+    await erc5727.deployed()
+    const [owner] = await ethers.getSigners()
+    await erc5727.__ERC5727Example_init(
+      owner.address,
+      'ERC5727Example',
+      'ERC5727',
+      [owner.address],
+      '',
+    )
+    await erc5727.mint(
+      owner.address,
+      210,
+      1,
+      ethers.BigNumber.from(Number.MAX_SAFE_INTEGER - 1),
+      false,
+    )
+    expect(await erc5727.balanceOf(owner.address)).to.equal(1)
+    expect(await erc5727.valueOf_(0)).to.equal(210)
+
+    const souldropFactory = await ethers.getContractFactory('Souldrop')
+    const souldrop = await souldropFactory.deploy()
+    await souldrop.deployed()
+    await souldrop.initialize(
+      owner.address,
+      10,
+      'https://api.soularis.io/souldrop',
+      erc5727.address,
+    )
+
+    expect(await souldrop.owner()).to.equal(owner.address)
+    expect(await souldrop.balanceOf(owner.address)).to.equal(1)
+  })
+})
