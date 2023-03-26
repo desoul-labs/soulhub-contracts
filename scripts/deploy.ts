3c4a12c67d7a7795103cfc9fe1584104e76f1235
Austin Zhu <austinzhu666@gmail.com>
2023-03-27 08:10:25 +0900

    feat: update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,31 +1,30 @@
-import { ethers, run } from 'hardhat'
+import { ethers, run } from 'hardhat';
 import {
   type SoulHubUpgradeable,
   SoulHubUpgradeable__factory,
   type TransparentUpgradeableProxy,
   type UpgradeableBeacon,
   type ERC5727SBTUpgradeable,
-} from '../typechain'
+} from '../typechain';
 
-async function sleep(ms: number): Promise<void> {
-  await new Promise((resolve) => setTimeout(resolve, ms));
+async function verifyContract(address: string, constructorArguments: any[]): Promise<void> {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  await run('verify:verify', {
+    address,
+    constructorArguments,
+  }).catch((error) => {
+    console.info(error);
+  });
 }
 
 async function deploySbt(): Promise<ERC5727SBTUpgradeable> {
-  const sbtContract = await ethers.getContractFactory('ERC5727SBTUpgradeable')
+  const sbtContract = await ethers.getContractFactory('ERC5727SBTUpgradeable');
 
   const sbt = await sbtContract.deploy();
   await sbt.deployed();
   console.log('SBT contract deployed to:', sbt.address);
 
-  await sleep(10000);
-  await run('verify:verify', {
-    address: sbt.address,
-    constructorArguments: [],
-  }).catch((error) => {
-    console.info(error);
-  });
-
+  await verifyContract(sbt.address, []);
   return sbt;
 }
 
@@ -36,33 +35,19 @@ async function deployBeacon(implementation: string): Promise<UpgradeableBeacon> 
   await beacon.deployed();
   console.log('Beacon contract deployed to:', beacon.address);
 
-  await sleep(10000);
-  await run('verify:verify', {
-    address: beacon.address,
-    constructorArguments: [implementation],
-  }).catch((error) => {
-    console.info(error);
-  });
-
+  await verifyContract(beacon.address, [implementation]);
   return beacon;
 }
 
-async function deploySoulHub(beacon: string): Promise<SoulHubUpgradeable> {
-  const soulHubContract = await ethers.getContractFactory('SoulHubUpgradeable')
-  const soulHub = await soulHubContract.deploy()
+async function deploySoulHub(): Promise<SoulHubUpgradeable> {
+  const soulHubContract = await ethers.getContractFactory('SoulHubUpgradeable');
+  const soulHub = await soulHubContract.deploy();
 
-  await soulHub.deployed()
-  console.log('SoulHub contract deployed to:', soulHub.address)
+  await soulHub.deployed();
+  console.log('SoulHub contract deployed to:', soulHub.address);
 
-  await sleep(10000);
-  await run('verify:verify', {
-    address: soulHub.address,
-    constructorArguments: [],
-  }).catch((error) => {
-    console.info(error);
-  });
-
-  return soulHub
+  await verifyContract(soulHub.address, []);
+  return soulHub;
 }
 
 async function deployTransparentProxy(
@@ -70,41 +55,35 @@ async function deployTransparentProxy(
   admin: string,
   data: string,
 ): Promise<TransparentUpgradeableProxy> {
-  const transparentProxyContract = await ethers.getContractFactory('TransparentUpgradeableProxy')
-  const transparentProxy = await transparentProxyContract.deploy(impl, admin, data)
+  const transparentProxyContract = await ethers.getContractFactory('TransparentUpgradeableProxy');
+  const transparentProxy = await transparentProxyContract.deploy(impl, admin, data);
 
   await transparentProxy.deployed();
   console.log('TransparentUpgradeableProxy contract deployed to:', transparentProxy.address);
 
-  await sleep(10000);
-  await run('verify:verify', {
-    address: transparentProxy.address,
-    constructorArguments: [impl, admin, data],
-  }).catch((error) => {
-    console.info(error);
-  });
-
+  await verifyContract(transparentProxy.address, [impl, admin, data]);
   return transparentProxy;
 }
 
 async function main(): Promise<void> {
-  const [hubAdmin, orgAdmin] = await ethers.getSigners()
-  console.log('Deploying contracts with the account:', hubAdmin.address)
-  console.log('Network:', (await ethers.provider.getNetwork()).name)
+  const [deployer] = await ethers.getSigners();
+  console.log('Deploying contracts with the account:', deployer.address);
+  console.log('Network:', (await ethers.provider.getNetwork()).name);
 
-  const sbtImpl = await deploySbt()
-  const beacon = await deployBeacon(sbtImpl.address)
+  const sbtImpl = await deploySbt();
+  const beacon = await deployBeacon(sbtImpl.address);
 
-  const soulHubImpl = await deploySoulHub(beacon.address)
+  const soulHubImpl = await deploySoulHub();
   const initData = SoulHubUpgradeable__factory.createInterface().encodeFunctionData(
     '__SoulHub_init',
     [beacon.address],
-  )
-  const soulHubProxy = await deployTransparentProxy(soulHubImpl.address, hubAdmin.address, initData)
-  const soulHub = SoulHubUpgradeable__factory.connect(soulHubProxy.address, orgAdmin)
-
-  const organization = await soulHub.callStatic.createOrganization('Desoul')
-  console.log('New organization:', organization)
+  );
+  const soulHubProxy = await deployTransparentProxy(
+    soulHubImpl.address,
+    deployer.address,
+    initData,
+  );
+  console.log('SoulHub proxy address:', soulHubProxy.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
