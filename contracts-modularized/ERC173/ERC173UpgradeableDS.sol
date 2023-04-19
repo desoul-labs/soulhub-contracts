6d5ef3c47a93f3ce81c8d525e87512c8a3afd53d
byeond009 <1249413181@qq.com>
2023-04-19 22:57:42 +0800

    feat: add diamond proxy

---
+++ contracts-modularized/ERC173/ERC173UpgradeableDS.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {LibDiamond} from "../diamond/libraries/LibDiamond.sol";
+import {IERC173} from "./interfaces/IERC173.sol";
+import "@openzeppelin/contracts-upgradeable/utils/introspection/ERC165Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
+
+contract OwnershipFacet is IERC173 {
+    function transferOwnership(address _newOwner) external override {
+        LibDiamond.enforceIsContractOwner();
+        LibDiamond.setContractOwner(_newOwner);
+    }
+
+    function owner() external view override returns (address owner_) {
+        owner_ = LibDiamond.contractOwner();
+    }
+}
